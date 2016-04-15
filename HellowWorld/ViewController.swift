836f458d53787a21b664ba3162cb46027d0e8ee8
Oliver Pajarillo <Oliver@oliver-air.local>
2016-04-15 16:21:00 +0300

    Initial Commit

---
+++ HellowWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HellowWorld
+//
+//  Created by Oliver Pajarillo on 4/15/16.
+//  Copyright © 2016 IOTrex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
